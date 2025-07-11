3a73630865453284dd68d4c14a110c7f8c855d23
forcaweb <forcawebsuporte@gmail.com>
2025-07-11 12:13:51 -0300

    Adicionado novas informações

--- js/page.js
+++ js/page.js
@@ -1,8 +1,3 @@
-document.addEventListener("DOMContentLoaded", () => {
-  showMenuMobile();
-  openChatFormBooking();
-});
-
 function showMenuMobile() {
   const btn = document.getElementById("btn-menu-left");
   const btnClose = document.getElementById("btn-close");
@@ -38,8 +33,38 @@ function openChatFormBooking() {
     const adultsQtds = e.currentTarget[2].value;
     const childrenQtds = e.currentTarget[3].value;
 
-    const numberWhatsapp = `https://wa.me/553591596756?text=Ol%C3%A1%2C%20estou%20vindo%20pelo%20site%20e%20gostaria%20de%20fazer%20uma%20reserva.%20Quero%20na%20Data%20${converteDate(checkin)}%20a%20${converteDate(checkout)}%20para%20${adultsQtds}%20adultos%20e%20${childrenQtds}%20crian%C3%A7as.`;
+    const numberWhatsapp = `https://wa.me/553591596756?text=Ol%C3%A1%2C%20estou%20vindo%20pelo%20site%20e%20gostaria%20de%20fazer%20uma%20reserva.%20Quero%20na%20Data%20${converteDate(
+      checkin
+    )}%20a%20${converteDate(
+      checkout
+    )}%20para%20${adultsQtds}%20adultos%20e%20${childrenQtds}%20crian%C3%A7as.`;
 
     window.location.href = numberWhatsapp;
   });
 }
+
+const swiperEl = document.querySelector(".reviews");
+Object.assign(swiperEl, {
+  slidesPerView: 1,
+  spaceBetween: 1,
+  breakpoints: {
+    640: {
+      slidesPerView: 2,
+      spaceBetween: 1,
+    },
+    768: {
+      slidesPerView: 4,
+      spaceBetween: 1,
+    },
+    1024: {
+      slidesPerView: 4,
+      spaceBetween: 1,
+    },
+  },
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  showMenuMobile();
+  openChatFormBooking();
+  swiperEl.initialize();
+});
